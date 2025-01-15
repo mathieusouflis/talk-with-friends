7c05a7cdcc9a9886d18d3260cca3e000db5d5c93
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-01-15 20:58:37 +0100

    cors config

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors"
 
 const app = express();
 const httpServer = createServer(app);
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  credentials: true
+}))
+
 const io = new Server(httpServer, {
     cors: {
         origin: "*",
